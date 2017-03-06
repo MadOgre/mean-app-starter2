dbd76ce5a5df61b23fcc4a7c5a0e76a50c9e0d39
Mike Semko <msemko@gmail.com>
2017-03-06 03:35:15 -0500

    removed old routes

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,17 @@
 "use strict";
 
-let categoriesController = require("./categories-controller");
-let listingsController = require("./listings-controller");
-let usersController = require("./users-controller");
-let authController = require("./auth-controller");
-let checkpointController = require("./checkpoint-controller");
+//declare your controllers here
+
+//let articlesController = require("./articles-controller");
 
 module.exports = app => {
 
-	app.get("/api/v1/categories", categoriesController.getAll);
-	app.get("/api/v1/category/:name", categoriesController.getOne);
-	app.post("/api/v1/category", checkpointController.verifyAdmin, categoriesController.create);
-	app.put("/api/v1/category/:name",  checkpointController.verifyAdmin, categoriesController.update);
-	app.delete("/api/v1/category/:name",  checkpointController.verifyAdmin, categoriesController.delete);
+	//All routes should go here
 
-	app.get("/api/v1/listings", listingsController.getAll);
-	app.get("/api/v1/listing/:id", listingsController.getOne);
-	app.post("/api/v1/listing", checkpointController.verifyUser, listingsController.create);
-	app.put("/api/v1/listing/:id", checkpointController.verifyOwner, listingsController.update);
-	app.delete("/api/v1/listing/:id", checkpointController.verifyOwner, listingsController.delete);
+	// app.get("/api/v1/articles", categoriesController.getAll);
+	// app.get("/api/v1/article/:name", categoriesController.getOne);
+	// app.post("/api/v1/article", categoriesController.create);
+	// app.put("/api/v1/article/:name", categoriesController.update);
+	// app.delete("/api/v1/article/:name", categoriesController.delete);
 
-	app.get("/api/v1/users", checkpointController.verifyAdmin, usersController.getAll);
-	app.get("/api/v1/user/:email", checkpointController.verifyAdmin, usersController.getOne);
-	app.post("/api/v1/user", checkpointController.verifyAdmin, usersController.create);
-	app.put("/api/v1/user/:email", checkpointController.verifyAdmin, usersController.update);
-	app.delete("/api/v1/user/:email", checkpointController.verifyAdmin, usersController.delete);
-
-	app.post("/login", authController.login);
 };
